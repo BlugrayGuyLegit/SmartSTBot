53c0c6d665f8e639e8fbb65f5c5d4284692e0d33
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-07-12 16:15:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,51 @@
+import os
 import discord
-import socket
+import requests
 
-# Configuration du bot Discord
-client = discord.Client()
+# Configuration de Discord
+intents = discord.Intents.default()
+intents.messages = True
+client = discord.Client(intents=intents)
 
-# Configuration de la connexion à ChatScript
-HOST = 'localhost'  # Adresse IP du serveur ChatScript
-PORT = 1024          # Port utilisé par le serveur ChatScript
+# Configuration de Wit.ai
+WIT_AI_TOKEN = "WH74G7K3UI4NCVC5M5M5PLAB2HEBIUKW"
+WIT_AI_API_URL = "https://api.wit.ai/message?v=20240712&q="
 
-def chat_with_chatscript(message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(message.encode())
-        response = s.recv(1024).decode()
-        return response
-
+# Événement de connexion du bot
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {client.user.name}')
 
+# Événement de réception de message sur Discord
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    # Appel à Wit.ai pour comprendre le message
+    response = wit_ai_request(message.content)
+    
+    # Récupérer la réponse de Wit.ai
+    if response:
+        await message.channel.send(response)
 
-    # Envoyer le message à ChatScript et recevoir la réponse
-    chat_response = chat_with_chatscript(message.content)
-    await message.channel.send(chat_response)
+# Fonction pour appeler l'API Wit.ai
+def wit_ai_request(message):
+    headers = {
+        'Authorization': f'Bearer {WIT_AI_TOKEN}',
+        'Content-Type': 'application/json'
+    }
+    params = {
+        'q': message
+    }
+    response = requests.get(WIT_AI_API_URL, headers=headers, params=params)
+    data = response.json()
+    
+    # Récupérer la réponse de Wit.ai
+    if 'intents' in data and data['intents']:
+        return data['intents'][0]['name']
+    else:
+        return "Désolé, je ne comprends pas."
 
-# Exécuter le bot Discord avec le token approprié
-client.run('DISCORD_BOT_TOKEN')
+# Démarrer le bot Discord
+client.run(os.getenv('DISCORD_BOT_TOKEN'))
